e0617c7b6422421aebf84fd9996823d7638bb6f9
MoeJoe950 <moe.joe.950@gmail.com>
2017-07-28 23:10:42 +0400

    greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -15,23 +15,36 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
 
+    socket.emit('newMessage', {
+        from: "Admin",
+        text: "Welcome to the chat app",
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: "Admin",
+        text: "New User Joined",
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
         });
+        socket.broadcast.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
-
-    // socket.on('createEmail', (newEmail) => {
-    //     console.log('createEmail', newEmail)
-    // });
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
 });
 
-server.listen(port, ()=> {
+server.listen(port, () => {
     console.log(`server is up on server ${port}`);
 });
