e6b327dc5cfddfd17ff9b992c9db9d2f264fb4f2
MoeJoe950 <moe.joe.950@gmail.com>
2017-07-29 19:00:32 +0400

    add geolocation support via geolocation API

--- public/js/index.js
+++ public/js/index.js
@@ -15,12 +15,40 @@ socket.on('newMessage', function (Message) {
     jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function(message) {
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank">My Current Location</a>');
+
+    li.text(`${message.from}`);
+    a.attr('href', message.url);
+
+    li.append(a);
+    jQuery('#messages').append(li);
+});
+
 jQuery('#message-form').on('submit', function (event) {
     event.preventDefault();
     socket.emit('createMessage', {
         from: 'User',
         text: jQuery('[name="messages"]').val()
     }, function () {
-
+        //FUNCTION HERE
     });
+});
+
+var locationButton = jQuery('#send-location');
+
+locationButton.on('click', function () {
+    if(!navigator.geolocation) {
+        return alert('GeoLocation not supported by your browser');
+    }
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    }, function () {
+        alert('unable to fetch location');
+    })
 });
