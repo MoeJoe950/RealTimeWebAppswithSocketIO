39bc422257430c627f1a401a0056160bab3110d5
MoeJoe950 <moe.joe.950@gmail.com>
2017-08-02 20:07:36 +0400

    Format Timestamps using momentJS

---
+++ playground/time.js
@@ -0,0 +1,10 @@
+var moment = require('moment');
+
+// var date = new Date();
+// var months = ['Jan', 'Feb', 'Mar',' Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+//
+// console.log(months[date.getMonth()]);
+
+var date = moment();
+console.log(date.format('MMM Do, YYYY'));
+
